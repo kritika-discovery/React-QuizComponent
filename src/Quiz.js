c0e79507954a8015be26681fe4f446e58da0d4f9
kritika-discovery <94848006+kritika-discovery@users.noreply.github.com>
2021-11-25 20:41:10 +0530

    initial commit

---
+++ src/Quiz.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+let quizData = require("./quiz_data.json");
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.setState = {
+      quiz_position: 1
+    };
+    console.log(quizData.quiz_questions[0].instruction_text);
+  }
+
+  render() {
+    var data = quizData.quiz_questions.map((item) => (
+      <div>{item.instruction_text}</div>
+    ));
+    return (
+      <div>
+        <div className="QuizQuestion">{data}</div>
+      </div>
+    );
+  }
+}
+
+export default Quiz;
