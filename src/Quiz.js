34c9dbc045346934ea636924101f45a443354aa4
kritika-discovery <94848006+kritika-discovery@users.noreply.github.com>
2021-11-26 12:29:36 +0530

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
-let quizData = require("./quiz_data.json");
+import React,{Component} from 'react';
+import QuizQuestion from './QuizQuestion';
 
+let quizData=require('./quiz_data.json');
 class Quiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quiz_position: 1
-    };
-    console.log(quizData.quiz_questions[0].instruction_text);
-  }
-
-   render() {
-    return (
+    constructor(props) {
+      super(props);
+      this.state = {
+        quiz_position: 1
+      };
+      console.log(quizData.quiz_questions[0].instruction_text);
+    }
+  
+    render() {
+      return(
       <div>
-        <div className="QuizQuestion">
-          {quizData.quiz_questions[0].instruction_text}
-        </div>
-      </div>
-    );
+      <QuizQuestion
+        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+      />
+    </div>
+      )}
   }
-}
-
-export default Quiz;
+  
+  export default Quiz;
